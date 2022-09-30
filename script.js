d3188bffcec7e3dfdadd9799566ef842d47d86a2
Segun Ajibola <ajbl.segun@gmail.com>
2022-09-30 15:56:50 +0100

    fixTopImages

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-
 async function getPhotos() {
-  // let response = await fetch("photo.json");
   let response = await fetch(
     "https://pixabay.com/api/?key=24194223-7c2f2d1f20c592a1f12e67655&q=yellow+flowers&image_type=photo"
   );
   let photos = await response.json();
-  console.log(photos.hits);
   return photos.hits;
 }
 
@@ -18,11 +15,9 @@ function getMyPhotosHtml(photos) {
   return `<div class="my-photos">${myPhotosHtml}</div>`;
 }
 
-//  src="https://picsum.photos/id/${photos[2].id}/300/300" />
-
 getPhotos().then((photos) => {
   let myPhotosHtml = getMyPhotosHtml(photos);
-  console.log(photos)
+  console.log(photos);
   document.body.innerHTML = `<div class="my-gallery">
         <img id="my-selected-photo" class="my-photo" src="${photos[0].largeImageURL}" />
         <div class="par">All Images: Select any images below to highlight</div>
@@ -34,11 +29,6 @@ getPhotos().then((photos) => {
 
   myPhotoImgs.forEach((photoImg) => {
     photoImg.addEventListener("click", (e) => {
-      
-      console.log("here", photoImg.src)
-      // get the selected photo src
-      
-      
       topPhoto.src = photoImg.src;
       topPhoto.style.display = "block";
     });
