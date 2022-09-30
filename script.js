183bfda15d4f16fc473c98b1e097b8f267382cb2
Segun Ajibola <ajbl.segun@gmail.com>
2022-09-30 02:36:10 +0100

    newAPI

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 async function getPhotos() {
   // let response = await fetch("photo.json");
-  let response = await fetch("https://pixabay.com/api/?key=24194223-7c2f2d1f20c592a1f12e67655&q=yellow+flowers&image_type=photo");
+  let response = await fetch(
+    "https://pixabay.com/api/?key=24194223-7c2f2d1f20c592a1f12e67655&q=yellow+flowers&image_type=photo"
+  );
   let photos = await response.json();
-  console.log(photos.hits)
+  console.log(photos.hits);
   return photos.hits;
 }
 
 function getMyPhotosHtml(photos) {
-  let myPhotosHtml = photos.map((photo) => {
+  let myPhotosHtml = photos
+    .map((photo) => {
       return `<img class="my-photo" src="${photo.webformatURL}" alt="${photo.user}" />`;
     })
     .join("");
@@ -18,8 +21,10 @@ function getMyPhotosHtml(photos) {
 
 getPhotos().then((photos) => {
   let myPhotosHtml = getMyPhotosHtml(photos);
+  console.log(photos)
+  console.log(photos[4].largeImageURL)
   document.body.innerHTML = `<div class="my-gallery">
-        <img id="my-selected-photo" class="my-photo" src="${photos.webformatURL}" />
+        <img id="my-selected-photo" class="my-photo" src="${photos[4].largeImageURL}" />
         ${myPhotosHtml}
     </div>`;
 
@@ -30,7 +35,7 @@ getPhotos().then((photos) => {
         photoImg.src.substr(0, photoImg.src.length - 7) + `300/300`;
       let selectedPhoto = document.getElementById("my-selected-photo");
       selectedPhoto.src = selectedPhotoSrc;
-      selectedPhoto.style.display = "inline";
+      // selectedPhoto.style.display = "inline";
     });
   });
 });
